ab31ef8303e27c632babad1d3eb7094cec2c099b
FRJ74 <frankr74@gmail.com>
2024-03-11 23:14:00 -0600

    Access an HTML element with JS

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 /*Access an HTML element */
 
 const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false;
+
+function cleanInputString(str) {
+
+}
