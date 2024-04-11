faff1a8683137f3a1007037366c0867d9588776b
FRJ74 <frankr74@gmail.com>
2024-04-11 15:56:35 -0600

    Add a new line after your second label, create another input element.

--- scripts.js
+++ scripts.js
@@ -22,5 +22,9 @@ function isInvalidInput(str) {
 function addEntry () {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-    const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>`; 
+    const HTMLString = 
+   `<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input for="${entryDropdown.value}-${entryNumber}-calories" type="number" min="0"  id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />`; 
 };
