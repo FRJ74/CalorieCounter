f23272294af1cd726d9555dcdd101ff8e3244da8
FRJ74 <frankr74@gmail.com>
2024-05-04 15:50:24 -0600

    Add <p> Calories Budgeted</p>

--- script.js
+++ script.js
@@ -52,9 +52,10 @@ function calculateCalories(e) {
   const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
   const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
   const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
-  output.innerHTML = ``
-
-
+  output.innerHTML = `<span class = "${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+  <hr>
+  
+  <p>${budgetCalories} Calories Budgeted</p>`;
 }
 
 function getCaloriesFromInputs(list) {
